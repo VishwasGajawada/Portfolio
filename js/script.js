8581a36ea46cd99137883d4f2470ff3eb272cbf7
VishwasGajawada <vishwasgajawada2002@gmail.com>
2021-05-19 22:04:12 +0530

    edits to navbar

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ const body = document.getElementsByTagName('body')[0];
 const nav_item = document.querySelectorAll("li a");
 
 function scrollfunc() {
-    let nav = document.querySelector('header');
+    let head = document.querySelector('header');
     // let windowPosition = window.scrollY > 0 && window.innerWidth > 600;
-    let windowPosition = window.scrollY > 0;;
-    nav.classList.toggle('scrolling-nav', windowPosition);
+    let windowPosition = (window.scrollY > 0  || nav.classList.contains("active"));
+    head.classList.toggle('scrolling-nav', windowPosition);
 } 
 window.addEventListener('scroll', scrollfunc);
 document.body.addEventListener('touchmove',scrollfunc);
@@ -17,15 +17,17 @@ document.body.addEventListener('touchmove',scrollfunc);
 ham.addEventListener('click',()=>{
     nav.classList.toggle('active');
     body.classList.toggle('hide');
+    // document.querySelector('header').classList.toggle('scrolling-nav');
     // header.classList.toggle('over-scroll');
-    console.log("ok");
-    // console.log(body);
+    scrollfunc();
+    console.log((nav.classList.contains("active")),nav.classList);
 });
 
 for(const ele of nav_item){
     ele.addEventListener('click',()=>{
         nav.classList.toggle('active');
         body.classList.toggle('hide');
+        scrollfunc();
         // header.classList.toggle('over-scroll');
     })
 }
